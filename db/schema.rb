9ed677ba6f36312c24e15c10f1b0c7b56dee92ee
jlbenavente <jlbenaventecanales@gmail.com>
2018-11-15 21:58:37 -0300

    cuarto commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_032545) do
+ActiveRecord::Schema.define(version: 2018_11_16_003426) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    
   end
 
 end
